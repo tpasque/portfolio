627c18e850708267feec24a972c92ac0e50d2a1b
Thomas Pasque <tpasque@gmail.com>
2016-04-25 10:01:24 -0600

    css updates for mobile responsiveness

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -1,3 +1,3 @@
 app.controller('MainController', ['$scope', function ($scope) {
-  console.log("you are using the main contoller");
+  console.log("Welcome to my Portfolio page.  Read on.  If you like what you see, I look forward to hearing from you! All the best, Tommy Pasque.");
 }])
